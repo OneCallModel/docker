83602084cf5e739de2bc1ba20010e852115815b2
Mr. Rajkumar Rajamanickam <sr_rajkumar@hotmail.com>
2026-05-31 18:03:10 +0530

    Updated
    - Started organizing new Docker project structure with best naming practices.
    - Added abstraction based directory structing for our workload Dockerfiles.
    - Included Apache 2.0 license file for compliance and regulatory approvals.
    - Written a README markdown file which can be used for documentation purpose.

--- workload/hadoop/2.10.1/name/script/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/local/lib/hadoop/sbin/hadoop-daemon.sh stop namenode
--- workload/kafka/3.2.1/controller/script/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SERVER_CONF=/opt/kafka/conf/server.conf
-/usr/local/lib/kafka/bin/kafka-server-stop.sh $SERVER_CONF
